503d23d68093fd2ff1afda5a8b3981fd5f414387
Colin <biz@colinhan.info>
2025-06-01 10:41:54 +0800

    Add lint staged, and other config.

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    "src/**/*.{js,ts,tsx}",
+    "!src/**/*.d.ts"
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
+module.exports = config;
